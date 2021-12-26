815b6f25ac70170b2c33c6d25c7517075deff38e
Nabil Mecili <nabilmecili@gmail.com>
2021-12-26 10:19:45 +0100

    Add Bootstrap

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -66,6 +66,7 @@ class RegistrationController extends AbstractController
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
+            'activee' => 'Register',
         ]);
     }
 
