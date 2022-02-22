19dfdb77951775385e685aa421b1a156cfef496d
Nabil Mecili <nabilmecili@gmail.com>
2022-02-23 00:56:41 +0100

    Changes needed for codacy

--- Dbase/snowtricks.sql
+++ Dbase/snowtricks.sql
@@ -18,27 +18,27 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `snowtricks`
+-- Base de données : 'snowtricks'
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `categories`
+-- Structure de la table 'categories'
 --
 
-DROP TABLE IF EXISTS `categories`;
-CREATE TABLE IF NOT EXISTS `categories` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `label` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS 'categories';
+CREATE TABLE IF NOT EXISTS 'categories' (
+  'id' int(11) NOT NULL AUTO_INCREMENT,
+  'label' varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY ('id')
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Déchargement des données de la table `categories`
+-- Déchargement des données de la table 'categories'
 --
 
-INSERT INTO `categories` (`id`, `label`) VALUES
+INSERT INTO 'categories' ('id', 'label') VALUES
 (1, 'Les grabs'),
 (2, 'Les rotations'),
 (3, 'Les flips'),
@@ -50,28 +50,28 @@ INSERT INTO `categories` (`id`, `label`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `comments`
+-- Structure de la table 'comments'
 --
 
-DROP TABLE IF EXISTS `comments`;
-CREATE TABLE IF NOT EXISTS `comments` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `users_id` int(11) NOT NULL,
-  `tricks_id` int(11) NOT NULL,
-  `content` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `author` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` datetime NOT NULL,
-  `modify_at` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `IDX_5F9E962A67B3B43D` (`users_id`),
-  KEY `IDX_5F9E962A3B153154` (`tricks_id`)
+DROP TABLE IF EXISTS 'comments';
+CREATE TABLE IF NOT EXISTS 'comments' (
+  'id' int(11) NOT NULL AUTO_INCREMENT,
+  'users_id' int(11) NOT NULL,
+  'tricks_id' int(11) NOT NULL,
+  'content' longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  'author' varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  'created_at' datetime NOT NULL,
+  'modify_at' datetime NOT NULL,
+  PRIMARY KEY ('id'),
+  KEY 'IDX_5F9E962A67B3B43D' ('users_id'),
+  KEY 'IDX_5F9E962A3B153154' ('tricks_id')
 ) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Déchargement des données de la table `comments`
+-- Déchargement des données de la table 'comments'
 --
 
-INSERT INTO `comments` (`id`, `users_id`, `tricks_id`, `content`, `author`, `created_at`, `modify_at`) VALUES
+INSERT INTO 'comments' ('id', 'users_id', 'tricks_id', 'content', 'author', 'created_at', 'modify_at') VALUES
 (1, 7, 3, 'Trop belle l\'image', 'mecbil', '2022-01-17 10:00:17', '2022-01-17 10:00:17'),
 (2, 7, 3, 'Les petit oiseaux', 'mecbil', '2022-01-29 10:16:55', '2022-01-29 10:16:55'),
 (3, 4, 3, 'Un comment de Marck', 'mecbil', '2022-01-29 10:33:03', '2022-01-29 10:33:03'),
@@ -94,22 +94,22 @@ INSERT INTO `comments` (`id`, `users_id`, `tricks_id`, `content`, `author`, `cre
 -- --------------------------------------------------------
 
 --
--- Structure de la table `doctrine_migration_versions`
+-- Structure de la table 'doctrine_migration_versions'
 --
 
-DROP TABLE IF EXISTS `doctrine_migration_versions`;
-CREATE TABLE IF NOT EXISTS `doctrine_migration_versions` (
-  `version` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
-  `executed_at` datetime DEFAULT NULL,
-  `execution_time` int(11) DEFAULT NULL,
-  PRIMARY KEY (`version`)
+DROP TABLE IF EXISTS 'doctrine_migration_versions';
+CREATE TABLE IF NOT EXISTS 'doctrine_migration_versions' (
+  'version' varchar(191) COLLATE utf8_unicode_ci NOT NULL,
+  'executed_at' datetime DEFAULT NULL,
+  'execution_time' int(11) DEFAULT NULL,
+  PRIMARY KEY ('version')
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Déchargement des données de la table `doctrine_migration_versions`
+-- Déchargement des données de la table 'doctrine_migration_versions'
 --
 
-INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+INSERT INTO 'doctrine_migration_versions' ('version', 'executed_at', 'execution_time') VALUES
 ('DoctrineMigrations\\Version20211220123835', NULL, NULL),
 ('DoctrineMigrations\\Version20211221145442', '2021-12-21 14:54:53', 134),
 ('DoctrineMigrations\\Version20211228105947', '2021-12-28 11:00:03', 381),
@@ -120,24 +120,24 @@ INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_
 -- --------------------------------------------------------
 
 --
--- Structure de la table `pictures`
+-- Structure de la table 'pictures'
 --
 
-DROP TABLE IF EXISTS `pictures`;
-CREATE TABLE IF NOT EXISTS `pictures` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `tricks_id` int(11) NOT NULL,
-  `label` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `link` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `IDX_8F7C2FC03B153154` (`tricks_id`)
+DROP TABLE IF EXISTS 'pictures';
+CREATE TABLE IF NOT EXISTS 'pictures' (
+  'id' int(11) NOT NULL AUTO_INCREMENT,
+  'tricks_id' int(11) NOT NULL,
+  'label' varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  'link' varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY ('id'),
+  KEY 'IDX_8F7C2FC03B153154' ('tricks_id')
 ) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Déchargement des données de la table `pictures`
+-- Déchargement des données de la table 'pictures'
 --
 
-INSERT INTO `pictures` (`id`, `tricks_id`, `label`, `link`) VALUES
+INSERT INTO 'pictures' ('id', 'tricks_id', 'label', 'link') VALUES
 (1, 2, 'Image 1', '720.jpg'),
 (6, 3, 'Image 6', 'rodeo.jpg'),
 (10, 2, 'image 12', '1080.jpg'),
@@ -153,55 +153,55 @@ INSERT INTO `pictures` (`id`, `tricks_id`, `label`, `link`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `reset_password_request`
+-- Structure de la table 'reset_password_request'
 --
 
-DROP TABLE IF EXISTS `reset_password_request`;
-CREATE TABLE IF NOT EXISTS `reset_password_request` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `selector` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `hashed_token` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `requested_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
-  `expires_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
-  PRIMARY KEY (`id`),
-  KEY `IDX_7CE748AA76ED395` (`user_id`)
+DROP TABLE IF EXISTS 'reset_password_request';
+CREATE TABLE IF NOT EXISTS 'reset_password_request' (
+  'id' int(11) NOT NULL AUTO_INCREMENT,
+  'user_id' int(11) NOT NULL,
+  'selector' varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  'hashed_token' varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  'requested_at' datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  'expires_at' datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  PRIMARY KEY ('id'),
+  KEY 'IDX_7CE748AA76ED395' ('user_id')
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Déchargement des données de la table `reset_password_request`
+-- Déchargement des données de la table 'reset_password_request'
 --
 
-INSERT INTO `reset_password_request` (`id`, `user_id`, `selector`, `hashed_token`, `requested_at`, `expires_at`) VALUES
+INSERT INTO 'reset_password_request' ('id', 'user_id', 'selector', 'hashed_token', 'requested_at', 'expires_at') VALUES
 (1, 4, 'WmJ0jB9e4vE8DT4LNjcp', '2mF1kV0+7zt73VlYdDnI0+hEX57CCbapRt+tr6edx4g=', '2021-12-22 09:21:30', '2021-12-22 10:21:30');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `tricks`
+-- Structure de la table 'tricks'
 --
 
-DROP TABLE IF EXISTS `tricks`;
-CREATE TABLE IF NOT EXISTS `tricks` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `users_id` int(11) NOT NULL,
-  `categories_id` int(11) NOT NULL,
-  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `author` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `content` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` datetime NOT NULL,
-  `modify_at` datetime NOT NULL,
-  `fitured_img` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `IDX_E1D902C167B3B43D` (`users_id`),
-  KEY `IDX_E1D902C1A21214B7` (`categories_id`)
+DROP TABLE IF EXISTS 'tricks';
+CREATE TABLE IF NOT EXISTS 'tricks' (
+  'id' int(11) NOT NULL AUTO_INCREMENT,
+  'users_id' int(11) NOT NULL,
+  'categories_id' int(11) NOT NULL,
+  'title' varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  'author' varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  'content' longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  'created_at' datetime NOT NULL,
+  'modify_at' datetime NOT NULL,
+  'fitured_img' longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY ('id'),
+  KEY 'IDX_E1D902C167B3B43D' ('users_id'),
+  KEY 'IDX_E1D902C1A21214B7' ('categories_id')
 ) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Déchargement des données de la table `tricks`
+-- Déchargement des données de la table 'tricks'
 --
 
-INSERT INTO `tricks` (`id`, `users_id`, `categories_id`, `title`, `author`, `content`, `created_at`, `modify_at`, `fitured_img`) VALUES
+INSERT INTO 'tricks' ('id', 'users_id', 'categories_id', 'title', 'author', 'content', 'created_at', 'modify_at', 'fitured_img') VALUES
 (1, 7, 1, 'Mute', 'mecbil', 'Saisie de la carre frontside (face avant ) de la planche entre les deux pieds avec la main avant', '2022-01-06 10:18:20', '2022-01-06 10:18:20', '720-1.jpg'),
 (2, 4, 1, 'Melancholie', 'mecbil', 'Saisie de la carre backside (arrière ) de la planche, entre les deux pieds, avec la main avant.', '2022-01-06 10:30:18', '2022-02-01 15:01:38', 'backflip-1.jpg'),
 (3, 7, 1, 'Indy', 'mecbil', 'Saisie de la carre frontside de la planche, entre les deux pieds, avec la main arrière', '2022-01-08 18:52:09', '2022-01-08 18:52:09', 'mute .jpeg'),
@@ -223,51 +223,51 @@ INSERT INTO `tricks` (`id`, `users_id`, `categories_id`, `title`, `author`, `con
 -- --------------------------------------------------------
 
 --
--- Structure de la table `users`
+-- Structure de la table 'users'
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `lastname` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `nickname` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `roles` json NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `is_verified` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UNIQ_1483A5E9E7927C74` (`email`)
+DROP TABLE IF EXISTS 'users';
+CREATE TABLE IF NOT EXISTS 'users' (
+  'id' int(11) NOT NULL AUTO_INCREMENT,
+  'lastname' varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  'nickname' varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  'email' varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  'roles' json NOT NULL,
+  'password' varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  'is_verified' tinyint(1) NOT NULL,
+  PRIMARY KEY ('id'),
+  UNIQUE KEY 'UNIQ_1483A5E9E7927C74' ('email')
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Déchargement des données de la table `users`
+-- Déchargement des données de la table 'users'
 --
 
-INSERT INTO `users` (`id`, `lastname`, `nickname`, `email`, `roles`, `password`, `is_verified`) VALUES
+INSERT INTO 'users' ('id', 'lastname', 'nickname', 'email', 'roles', 'password', 'is_verified') VALUES
 (4, 'Dubois', 'marck', 'coucou@moi.fr', '[\"ROLE_USER\"]', '$2y$13$ukyweQB4WA0T/0HNvQTF2OvzCnSMFzSlaCiOjtgZqGcSGPksz50Im', 1),
 (7, 'Mecili', 'mecbil', 'contact@snowtricks.org', '[\"ROLE_ADMIN\"]', '$2y$13$krulME0mnpHnLnHYGGageOhuE8JvC7P5Cm/KEelzBRUE442fZSt1q', 1);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `vids`
+-- Structure de la table 'vids'
 --
 
-DROP TABLE IF EXISTS `vids`;
-CREATE TABLE IF NOT EXISTS `vids` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `tricks_id` int(11) NOT NULL,
-  `label` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `link` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `IDX_E08B5ED63B153154` (`tricks_id`)
+DROP TABLE IF EXISTS 'vids';
+CREATE TABLE IF NOT EXISTS 'vids' (
+  'id' int(11) NOT NULL AUTO_INCREMENT,
+  'tricks_id' int(11) NOT NULL,
+  'label' varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  'link' varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY ('id'),
+  KEY 'IDX_E08B5ED63B153154' ('tricks_id')
 ) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Déchargement des données de la table `vids`
+-- Déchargement des données de la table 'vids'
 --
 
-INSERT INTO `vids` (`id`, `tricks_id`, `label`, `link`) VALUES
+INSERT INTO 'vids' ('id', 'tricks_id', 'label', 'link') VALUES
 (2, 3, 'Video 2', 'https://www.youtube.com/embed/51sQRIK-TEI'),
 (4, 3, 'Video 4', 'https://www.youtube.com/embed/CA5bURVJ5zk'),
 (7, 3, 'trriioiozierzr', 'https://www.youtube.com/embed/6yA3XqjTh_w'),
@@ -283,36 +283,36 @@ INSERT INTO `vids` (`id`, `tricks_id`, `label`, `link`) VALUES
 --
 
 --
--- Contraintes pour la table `comments`
+-- Contraintes pour la table 'comments'
 --
-ALTER TABLE `comments`
-  ADD CONSTRAINT `FK_5F9E962A3B153154` FOREIGN KEY (`tricks_id`) REFERENCES `tricks` (`id`),
-  ADD CONSTRAINT `FK_5F9E962A67B3B43D` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`);
+ALTER TABLE 'comments'
+  ADD CONSTRAINT 'FK_5F9E962A3B153154' FOREIGN KEY ('tricks_id') REFERENCES 'tricks' ('id'),
+  ADD CONSTRAINT 'FK_5F9E962A67B3B43D' FOREIGN KEY ('users_id') REFERENCES 'users' ('id');
 
 --
--- Contraintes pour la table `pictures`
+-- Contraintes pour la table 'pictures'
 --
-ALTER TABLE `pictures`
-  ADD CONSTRAINT `FK_8F7C2FC03B153154` FOREIGN KEY (`tricks_id`) REFERENCES `tricks` (`id`) ON DELETE CASCADE;
+ALTER TABLE 'pictures'
+  ADD CONSTRAINT 'FK_8F7C2FC03B153154' FOREIGN KEY ('tricks_id') REFERENCES 'tricks' ('id') ON DELETE CASCADE;
 
 --
--- Contraintes pour la table `reset_password_request`
+-- Contraintes pour la table 'reset_password_request'
 --
-ALTER TABLE `reset_password_request`
-  ADD CONSTRAINT `FK_7CE748AA76ED395` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+ALTER TABLE 'reset_password_request'
+  ADD CONSTRAINT 'FK_7CE748AA76ED395' FOREIGN KEY ('user_id') REFERENCES 'users' ('id');
 
 --
--- Contraintes pour la table `tricks`
+-- Contraintes pour la table 'tricks'
 --
-ALTER TABLE `tricks`
-  ADD CONSTRAINT `FK_E1D902C167B3B43D` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `FK_E1D902C1A21214B7` FOREIGN KEY (`categories_id`) REFERENCES `categories` (`id`);
+ALTER TABLE 'tricks'
+  ADD CONSTRAINT 'FK_E1D902C167B3B43D' FOREIGN KEY ('users_id') REFERENCES 'users' ('id'),
+  ADD CONSTRAINT 'FK_E1D902C1A21214B7' FOREIGN KEY ('categories_id') REFERENCES 'categories' ('id');
 
 --
--- Contraintes pour la table `vids`
+-- Contraintes pour la table 'vids'
 --
-ALTER TABLE `vids`
-  ADD CONSTRAINT `FK_E08B5ED63B153154` FOREIGN KEY (`tricks_id`) REFERENCES `tricks` (`id`) ON DELETE CASCADE;
+ALTER TABLE 'vids'
+  ADD CONSTRAINT 'FK_E08B5ED63B153154' FOREIGN KEY ('tricks_id') REFERENCES 'tricks' ('id') ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
